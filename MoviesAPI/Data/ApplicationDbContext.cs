02934da9bcf00f6c843bc1f373a31e6163985882
Promise30 <promiseadeagbo@gmail.com>
2023-07-24 11:46:06 +0100

    Add project files.

---
+++ MoviesAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Range(0.0, 10.0, ErrorMessage = "Rating must be between 0.0 and 10.0")]
+        public decimal Rating { get; set; }
+
+    }
+}
