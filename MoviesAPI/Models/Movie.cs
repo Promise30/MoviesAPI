f399e828a9db2f1e8065400176959d3a0908369d
Promise30 <promiseadeagbo@gmail.com>
2023-07-24 12:53:40 +0100

    initia commit

--- MoviesAPI/Models/Movie.cs
+++ MoviesAPI/Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MoviesAPI.Models
 {
@@ -8,7 +10,7 @@ namespace MoviesAPI.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Director { get; set; }
-        [DataType(DataType.Date)]
+        [Column(TypeName = "date")]
         public DateTime ReleaseDate { get; set; }
         [Range(0.0, 10.0, ErrorMessage = "Rating must be between 0.0 and 10.0")]
         public decimal Rating { get; set; }
